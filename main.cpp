7b9bde1c168c2f71802997c32908973fc2d376f6
23520270 <23520270@gm.uit.edu.vn>
2024-11-10 14:42:26 +0700

    Chinh sua toc do phu hop sau khi to mau

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@ const int width = 80;
 const int height = 20;
 
 // Snake head coordinates of snake (x-axis, y-axis)
-Snake snake(width / 2, height / 2);
+Snake snake; 
+Point pre_snakeTail;
 // Food coordinates
 Food food;
 // variable to store the score of he player
@@ -26,187 +27,187 @@ bool isGameOver;
 // Hàm ẩn con trỏ nháy trong console
 void HideCursor()
 {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO cursorInfo;
-    GetConsoleCursorInfo(hConsole, &cursorInfo);
-    cursorInfo.bVisible = false; // Ẩn con trỏ
-    SetConsoleCursorInfo(hConsole, &cursorInfo);
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cursorInfo;
+	GetConsoleCursorInfo(hConsole, &cursorInfo);
+	cursorInfo.bVisible = false; // Ẩn con trỏ
+	SetConsoleCursorInfo(hConsole, &cursorInfo);
 }
 
-void setConsoleBackgroundColor(int r, int g, int b, std::string str) 
+void setConsoleBackgroundColor(int r, int g, int b, std::string str)
 {
-    std::cout << "\033[48;2;" << r << ";" << g << ";" << b << "m"; 
-    std::cout << str;
+	std::cout << "\033[48;2;" << r << ";" << g << ";" << b << "m";
+	std::cout << str;
 }
 
-// Function to initialize game variables
+void GoToXY(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+// Function to initialize game
 void GameInit()
 {
-    isGameOver = false;
-    food = Food(rand() % width, rand() % height);
-    playerScore = 0;
-    HideCursor(); // Ẩn con trỏ nháy
-    snakeTailLen = snake.getBody().size(); // Độ dài đuôi ban đầu của rắn 
+	GoToXY(0, 0);
+	// Creating top walls
+	for (int i = 0; i < width + 2; i++)
+		setConsoleBackgroundColor(79, 79, 79, " ");
+	cout << endl;
+
+	// Creating bottom walls
+	GoToXY(0, height + 1);
+	for (int i = 0; i < width + 2; i++)
+		cout << " ";
+	cout << endl;
+
+	// Creating side walls
+	for (int i = 1; i < height + 1; ++i)
+	{
+		GoToXY(0, i); cout << " ";
+		GoToXY(width + 1, i); cout << " ";
+	}
+
+	// Creating play field
+	for (int i = 1; i <= height; i++)
+		for (int j = 1; j <= width; j++)
+		{
+			GoToXY(j, i);
+			setConsoleBackgroundColor(0, 139, 0, " ");
+		}
+
+	snake = Snake(width / 2, height / 2);
+	isGameOver = false;
+	food = Food(rand() % width + 1, rand() % height + 1);
+	playerScore = 0;
+	HideCursor(); // Ẩn con trỏ nháy
+	snakeTailLen = snake.getBody().size(); // Độ dài đuôi ban đầu của rắn 
 }
 
-void GoToXY(int x, int y) 
-{
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-
-// Function for creating the game board & rendering
+// Function for creating snake
 void GameRender(string playerName)
 {
-    GoToXY(0, 0);
-    // Creating top walls with '='
-    for (int i = 0; i < width + 2; i++)
-        setConsoleBackgroundColor(79, 79, 79, " ");
-    cout << endl;
+	GoToXY(pre_snakeTail.getX(), pre_snakeTail.getY());
+	setConsoleBackgroundColor(0, 139, 0, " ");
 
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j <= width; j++) {
-            // Creating side walls
-            if (j == 0 || j == width) {
-                setConsoleBackgroundColor(79, 79, 79, " ");
-            }
-            // Creating snake's head
-            if (snake.getHead() == Point(j, i)) {
-                setConsoleBackgroundColor(105, 105, 105, " ");
-            }
+	GoToXY(snake.getHead().getX(), snake.getHead().getY());
+	setConsoleBackgroundColor(105, 105, 105, " ");
 
-            // Creating the sanke's food with '#'
-            else if (food.getFood() == Point(j, i)) {
-                setConsoleBackgroundColor(255, 69, 0, " ");
-            }
-            else {
-                bool prTail = false;
-                for (int k = 0; k < snakeTailLen; k++) {
-                    if (snake.getBody()[k] == Point(j, i)) {
-                        setConsoleBackgroundColor(0, 0, 0, " ");
-                        prTail = true;
-                    }
-                }
-                if (!prTail && j != width)
-                {
-                        setConsoleBackgroundColor(0, 139, 0, " ");
-                }
-            }
-        }
-        cout << endl;
-
-    }
-
-    // Creating bottom walls with '='
-    for (int i = 0; i < width + 2; i++)
-    {
-        setConsoleBackgroundColor(79, 79, 79, " ");
-    }
-    cout << endl;
-    // Display player's score
-    setConsoleBackgroundColor(0, 0, 0, "");
-    cout << playerName << "'s Score: " << playerScore
-        << endl;
+	GoToXY(food.getFood().getX(), food.getFood().getY());
+	setConsoleBackgroundColor(255, 69, 0, " ");
+	for (int k = 0; k < snakeTailLen; k++) {
+		GoToXY(snake.getBody()[k].getX(), snake.getBody()[k].getY());
+		setConsoleBackgroundColor(0, 0, 0, " ");
+	}
+	cout << endl;
+	// Display player's score
+	GoToXY(0, height + 2);
+	setConsoleBackgroundColor(0, 0, 0, "");
+	cout << playerName << "'s Score: " << playerScore << endl;
 }
 
 // Function for updating the game state
 void UpdateGame()
 {
-    snakeTailLen = snake.getBody().size();
-    snake.move(snake.getDirection());
-    // Checks for snake's collision with the wall (|)
-    if (snake.getHead().getX() >= width || snake.getHead().getX() < 0 || snake.getHead().getY() >= height || snake.getHead().getY() < 0)
-        isGameOver = true;
+	snakeTailLen = snake.getBody().size();
+	snake.move(snake.getDirection());
+	// Checks for snake's collision with the wall (|)
+	if (snake.getHead().getX() > width || snake.getHead().getX() == 0 || snake.getHead().getY() > height || snake.getHead().getY() == 0)
+		isGameOver = true;
 
-    // Checks for collision with the tail (o)
-    for (int i = 0; i < snakeTailLen; i++) {
-        if (snake.getBody()[i] == snake.getHead())
-            isGameOver = true;
-    }
+	// Checks for collision with the tail (o)
+	for (int i = 0; i < snakeTailLen; i++) {
+		if (snake.getBody()[i] == snake.getHead())
+			isGameOver = true;
+	}
 
-    // Checks for snake's collision with the food (#)
-    if (snake.foodCollision(food)) {
-        playerScore += 10;
-        food = Food(rand() % width, rand() % height);
-    }
+	// Checks for snake's collision with the food (#)
+	if (snake.foodCollision(food)) {
+		playerScore += 10;
+		food = Food(rand() % width + 1, rand() % height + 1);
+	}
 }
 
 // Function to set the game difficulty level
 int SetDifficultyLevel()
 {
-    int dfc;
-    char choice;
-    cout << "\nSET DIFFICULTY\n1: Easy\n2: Medium\n3: hard "
-        "\nNOTE: if not chosen or pressed any other key, the difficulty will be automatically\n     set to medium \nChoose difficulty level: ";
-    cin >> choice;
-    switch (choice) {
-    case '1':
-        dfc = 150; // for easy 
-        break;
-    case '2':
-        dfc = 100; // medium 
-        break;
-    case '3':
-        dfc = 50; // higher for hard 
-        break;
-    default:
-        dfc = 100; // default to medium 
-    }
-    return dfc;
+	int dfc;
+	char choice;
+	cout << "\nSET DIFFICULTY\n1: Easy\n2: Medium\n3: Hard "
+		"\nNOTE: if not chosen or pressed any other key, the difficulty will be automatically set to medium\nChoose difficulty level: ";
+	cin >> choice;
+	switch (choice) {
+	case '1':
+		dfc = 150; // for easy 
+		break;
+	case '2':
+		dfc = 100; // medium 
+		break;
+	case '3':
+		dfc = 50; // higher for hard 
+		break;
+	default:
+		dfc = 100; // default to medium 
+	}
+	return dfc;
 }
 
 // Function to handle user UserInput
 void UserInput()
 {
-    // Checks if a key is pressed or not
-    if (_kbhit()) {
-        int key = _getch();
-        if (key == 224) { // 224 cho phím mũi tên 
-            key = _getch(); // lấy mã tiếp theo để xđ hướng 
-            // Getting the pressed key
-            switch (key) {
-            case 75: // left 
-                if (snake.getDirection() != DirectionRight) snake.setDirection(DirectionLeft);
-                break;
-            case 77: // right 
-                if (snake.getDirection() != DirectionLeft) snake.setDirection(DirectionRight);
-                break;
-            case 72: // up 
-                if (snake.getDirection() != DirectionDown) snake.setDirection(DirectionUp);
-                break;
-            case 80: // down 
-                if (snake.getDirection() != DirectionUp) snake.setDirection(DirectionDown);
-                break;
-            }
-        }
-        else if (key == 'x') isGameOver = true;
-    }
+	// Checks if a key is pressed or not
+	if (_kbhit()) {
+		int key = _getch();
+		if (key == 224) { // 224 cho phím mũi tên 
+			key = _getch(); // lấy mã tiếp theo để xđ hướng 
+			// Getting the pressed key
+			switch (key) {
+			case 75: // left 
+				if (snake.getDirection() != DirectionRight) snake.setDirection(DirectionLeft);
+				break;
+			case 77: // right 
+				if (snake.getDirection() != DirectionLeft) snake.setDirection(DirectionRight);
+				break;
+			case 72: // up 
+				if (snake.getDirection() != DirectionDown) snake.setDirection(DirectionUp);
+				break;
+			case 80: // down 
+				if (snake.getDirection() != DirectionUp) snake.setDirection(DirectionDown);
+				break;
+			}
+		}
+		else if (key == 'x') isGameOver = true;
+	}
 }
 
 // Main function / game looping function
 int main()
 {
-    srand(time(NULL)); // seed for random number generation 
-    string playerName;
-    cout << "enter your name: ";
-    cin >> playerName;
-    int dfc = SetDifficultyLevel();
-
-    GameInit();
-    while (!isGameOver) {
-        GameRender(playerName);
-        UserInput();
-        UpdateGame();
-        // creating a delay for according to the chosen
-        // difficulty
-        if (snake.getDirection() == DirectionUp || snake.getDirection() == DirectionDown)
-            Sleep(dfc * 1.5);
-        else
-            Sleep(dfc);
-    }
-    system("cls");
-    cout << "Game Over!" << playerName << "'s final score: " << playerScore << endl;
-    return 0;
+	srand(time(NULL)); // seed for random number generation 
+	string playerName;
+	cout << "Enter your name: ";
+	cin >> playerName;
+	int dfc = SetDifficultyLevel();
+	system("cls");
+	GameRender(playerName);
+	GameInit();
+	while (!isGameOver) {
+		pre_snakeTail = snake.getTail();
+		UpdateGame();
+		if (isGameOver) break;
+		GameRender(playerName);
+		UserInput();
+		
+		// creating a delay for according to the chosen
+		// difficulty
+		if (snake.getDirection() == DirectionUp || snake.getDirection() == DirectionDown)
+			Sleep(dfc * 1.8);
+		else
+			Sleep(dfc);
+	}
+	system("cls");
+	cout << "Game Over!" << playerName << "'s final score: " << playerScore << endl;
+	return 0;
 }
